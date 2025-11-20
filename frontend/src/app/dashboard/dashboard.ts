c76968697248de477f858db29d5ddcad0751f43c
aashutosh kushavaha sa <aashutosh.kushavaha.sa@gmail.com>
2025-11-20 18:10:27 +0530

    feat(frontend): create admin dashboard and integrate API data

--- frontend/src/app/dashboard/dashboard.ts
+++ frontend/src/app/dashboard/dashboard.ts
@@ -1,18 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from "../sidebar/sidebar";
+import { SidebarComponent } from '../sidebar/sidebar';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [CommonModule, SidebarComponent],
   templateUrl: './dashboard.html',
-  styleUrls: ['./dashboard.css']
+  styleUrls: ['./dashboard.css'],
 })
-export class DashboardComponent {
-
-  totalEmployees = 42;
+export class DashboardComponent implements OnInit {
   activeEmployees = 38;
-  departments = 6;
 
+  http = inject(HttpClient);
+  employeeList: any[] = [];
+  totalEmployees = 0;
+  allDepartments : any[] = [];
+  uniqueDepartments : any[] = [];
+  departments = 0;
+
+  ngOnInit(): void {
+    this.getEmployee();
+  }
+
+  getEmployee() {
+    const token = localStorage.getItem('token');
+
+    const headers = new HttpHeaders({
+      Authorization: token ?? '',
+    });
+
+    this.http
+      .get('http://localhost:3000/api/employee/getall', { headers })
+      .subscribe((res: any) => {
+        this.employeeList = res;
+        this.totalEmployees = this.employeeList.length;
+        console.log('Employee List:', this.totalEmployees);
+
+        // fetch Department from employeeList
+        this.allDepartments = this.employeeList.map((emp) => emp.department);
+        // Get Unique Departmnet
+        this.uniqueDepartments = [...new Set(this.allDepartments)];
+
+        this.departments = this.uniqueDepartments.length;
+      });
+  }
 }
