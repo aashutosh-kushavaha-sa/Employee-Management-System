cba72ee62818f5e096acb164e7d45d8136ff72ad
aashutosh kushavaha sa <aashutosh.kushavaha.sa@gmail.com>
2025-11-24 12:51:45 +0530

    Replace Alert with Modal

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { ModalService } from '../modal/alertModel/modal.service';
 
 @Component({
   selector: 'app-login',
@@ -19,7 +20,7 @@ export class Login {
 
   private apiUrl = 'http://localhost:3003/api/auth/login';
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private router: Router,private modal: ModalService) {}
 
   submitted: boolean = false;
   loading: boolean = false;
@@ -52,7 +53,7 @@ export class Login {
           err.error && err.error.message
             ? err.error.message
             : 'Login failed due to a server error.';
-        alert('Login failed due to a server error.');
+        this.modal.show('Login failed due to a server error.', 'error');
       },
     });
   }
