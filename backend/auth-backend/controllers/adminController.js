4db05f5320e1591957a9cda98e374b12e171b23e
aashutosh kushavaha sa <aashutosh.kushavaha.sa@gmail.com>
2025-11-26 12:07:05 +0530

    refactor(backend): clean and improve controller structure for better readability

--- backend/auth-backend/controllers/adminController.js
+++ backend/auth-backend/controllers/adminController.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 
 // Generate JWT Token
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
 };
 
 
