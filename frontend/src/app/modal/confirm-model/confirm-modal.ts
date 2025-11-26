9d16cb6c97a0553f8564563336139fd4a23c20f0
aashutosh kushavaha sa <aashutosh.kushavaha.sa@gmail.com>
2025-11-26 12:03:05 +0530

    enhancement: refine data model and enhance UI styling and layout

--- frontend/src/app/modal/confirm-model/confirm-modal.ts
+++ frontend/src/app/modal/confirm-model/confirm-modal.ts
@@ -2,17 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfirmModalService } from './confirm-modal.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import {
-  faTriangleExclamation
-} from '@fortawesome/free-solid-svg-icons';
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-confirm-modal',
   standalone: true,
-  imports: [CommonModule,FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './confirm-modal.html'
 })
 export class ConfirmModalComponent {
   constructor(public modal: ConfirmModalService) {}
+
   warning = faTriangleExclamation;
 }
