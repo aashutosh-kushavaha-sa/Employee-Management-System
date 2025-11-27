d719e4a6744df6445e010626a30ed1413ef4911e
aashutosh kushavaha sa <aashutosh.kushavaha.sa@gmail.com>
2025-11-27 13:15:56 +0530

    refactor: clean up codebase by fixing warnings and removing unused code

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,15 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { SignUpComponent } from './sign-up/sign-up';
-import { Login } from "./login/login";
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { SidebarComponent } from "./sidebar/sidebar";
-import { DashboardComponent } from "./dashboard/dashboard";
+import { RouterOutlet } from '@angular/router';
 import { ModalComponent } from "./modal/alertModel/modal.component";
 import { ConfirmModalComponent } from "./modal/confirm-model/confirm-modal";
 
 @Component({
   selector: 'app-root',
-  imports: [Login, SignUpComponent, RouterOutlet, RouterLink, SidebarComponent, DashboardComponent, ModalComponent, ConfirmModalComponent],
+  imports: [RouterOutlet, ModalComponent, ConfirmModalComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
