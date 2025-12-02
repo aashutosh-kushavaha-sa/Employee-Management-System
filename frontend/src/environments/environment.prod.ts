a18793ff2ed44fe532d1f698b45b8239ef00ae19
aashutosh kushavaha sa <aashutosh.kushavaha.sa@gmail.com>
2025-12-02 10:06:46 +0530

    Change apiUrl

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://employee-management-system-xmfp.onrender.com'
+  apiUrl: 'https://muddy-magdalene-aashutosh-6575fafb.koyeb.app/'
 };
